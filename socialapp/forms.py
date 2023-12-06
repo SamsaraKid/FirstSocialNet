6a464c0f048df03670adf0d640a18519c1b0d9f7
koany <koroliev@yandex.ru>
2023-12-06 19:12:37 +0300

    поработал над регистрацией и стилями, последним добавил город

--- socialapp/forms.py
+++ socialapp/forms.py
@@ -1,7 +1,28 @@
+import datetime
+
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import *
+from django.utils.translation import gettext_lazy as _
+
+months = {
+    1: _("Январь"),
+    2: _("Февраль"),
+    3: _("Март"),
+    4: _("Апрель"),
+    5: _("Май"),
+    6: _("Июнь"),
+    7: _("Июль"),
+    8: _("Август"),
+    9: _("Сентябрь"),
+    10: _("Октябрь"),
+    11: _("Ноябрь"),
+    12: _("Декабрь"),
+}
+
+this_year = datetime.date.today().year
+years = range(this_year, this_year - 100, -1)
 
 
 class LogInForm(forms.Form):
@@ -10,14 +31,20 @@ class LogInForm(forms.Form):
 
 
 class SignUp(UserCreationForm):
-    name = forms.CharField(label='Имя', help_text='')
-    surname = forms.CharField(label='Фамилия', help_text='')
     username = forms.CharField(label='Имя пользователя', help_text='')
     password1 = forms.CharField(label='Пароль', help_text='',
                                 widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}))
     password2 = forms.CharField(label='Подтверждение пароля', help_text='',
                                 widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}))
     email = forms.EmailField(label='Почта', widget=forms.TextInput(attrs={'placeholder': 'name@mail.com'}))
+    name = forms.CharField(label='Имя', help_text='')
+    surname = forms.CharField(label='Фамилия', help_text='')
+    secondname = forms.CharField(label='Отчество', help_text='', required=False)
+    birthdate = forms.DateField(label='Дата рождения',
+                                widget=forms.SelectDateWidget(months=months,
+                                                              years=years))
+    city = forms.ModelChoiceField(label='Город', queryset=City.objects.all())
+
     avatar = forms.ImageField(label='Аватар')
 
     def clean_avatar(self):
